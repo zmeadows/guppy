dd8ee0a26bda1d65984898cc124e9c4a352abd4d
Zachary Meadows <zmeadows@gmail.com>
2015-12-29 12:39:14 -0500

    first IR output working

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,55 +1,121 @@
 #include "codegen.h"
 
-
-void
-FunctionGen::apply(const DefnASTNode &double_expr)
+llvm::Function*
+FunctionGen::process_prototype(const PrototypeAST &proto)
 {
+    assert(result == nullptr);
+
+    std::vector<llvm::Type*> type_vector(proto.args.size(),
+            llvm::Type::getDoubleTy(llvm::getGlobalContext()));
+
+    llvm::FunctionType *func_type = llvm::FunctionType::get(
+            llvm::Type::getDoubleTy(llvm::getGlobalContext()), type_vector, false);
+
+    llvm::Function* func = llvm::Function::Create(func_type,
+            llvm::Function::ExternalLinkage, proto.name, context->llvm_module.get());
+
+    unsigned int i = 0;
+    for (auto &f_arg : func->args())
+    {
+        f_arg.setName(proto.args[i++]);
+    }
+
+    return func;
 }
 
 void
-ValueGen::apply(const VariableASTExpr &var_expr)
+FunctionGen::apply_to(const ExternASTNode &extern_expr)
 {
-    assert(value == nullptr);
+    assert(result == nullptr);
 
-    auto it = generator->named_values->find(var_expr.name);
+    result = process_prototype(*extern_expr.prototype);
+}
 
-    if (it != generator->named_values->end()) {
-        value = it->second;
+void
+FunctionGen::apply_to(const DefnASTNode &defn_expr)
+{
+    assert(result == nullptr);
+
+    llvm::Function* function = context->llvm_module->getFunction(defn_expr.prototype->name);
+
+    if (function == nullptr) {
+        function = process_prototype(*defn_expr.prototype);
+    }
+
+    if (function == nullptr) {
+        throw 0;
+    }
+
+
+    llvm::BasicBlock *bb = llvm::BasicBlock::Create(llvm::getGlobalContext(), "entry", function);
+    context->builder.SetInsertPoint(bb);
+
+    context->named_values.clear();
+    for (auto &farg : function->args())
+    {
+        context->named_values[farg.getName()] = &farg;
+    }
+
+    ValueGen body_gen(context);
+    defn_expr.body->inject(body_gen);
+
+    llvm::Value* func_return_value = body_gen.extract();
+    if (func_return_value != nullptr)
+    {
+        context->builder.CreateRet(func_return_value);
+        llvm::verifyFunction(*function);
+        result = function;
+    } else {
+        function->eraseFromParent();
+        throw -1;
+    }
+}
+
+
+void
+ValueGen::apply_to(const VariableASTExpr &var_expr)
+{
+    assert(result == nullptr);
+
+    auto it = context->named_values.find(var_expr.name);
+
+    if (it != context->named_values.end()) {
+        result = it->second;
     } else {
         throw 0;
     }
 }
 
 void
-ValueGen::apply(const LiteralDoubleASTExpr &double_expr)
+ValueGen::apply_to(const LiteralDoubleASTExpr &double_expr)
 {
-    assert(value == nullptr);
+    assert(result == nullptr);
 
-    value = llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(double_expr.value));
+    result = llvm::ConstantFP::get(llvm::getGlobalContext(), llvm::APFloat(double_expr.value));
 }
 
 void
-ValueGen::apply(const BinOpASTExpr &binop_expr)
+ValueGen::apply_to(const BinOpASTExpr &binop_expr)
 {
-    assert(value == nullptr);
+    assert(result == nullptr);
 
-    ValueGen lhs_valgen(generator);
+    ValueGen lhs_valgen(context);
     binop_expr.LHS->inject(lhs_valgen);
-    llvm::Value* lhs_val = lhs_valgen.get();
+    llvm::Value* lhs_val = lhs_valgen.extract();
 
-    ValueGen rhs_valgen(generator);
-    binop_expr.RHS->inject(lhs_valgen);
-    llvm::Value* rhs_val = rhs_valgen.get();
+    ValueGen rhs_valgen(context);
+    binop_expr.RHS->inject(rhs_valgen);
+    llvm::Value* rhs_val = rhs_valgen.extract();
 
     if (binop_expr.binop == "+") {
-        value = generator->builder->CreateFAdd(lhs_val, rhs_val, "addtmp");
+        result = context->builder.CreateFAdd(lhs_val, rhs_val, "addtmp");
     } else if (binop_expr.binop == "-") {
-        value = generator->builder->CreateFSub(lhs_val, rhs_val, "subtmp");
+        result = context->builder.CreateFSub(lhs_val, rhs_val, "subtmp");
     } else if (binop_expr.binop == "*") {
-        value = generator->builder->CreateFMul(lhs_val, rhs_val, "multmp");
+        result = context->builder.CreateFMul(lhs_val, rhs_val, "multmp");
     } else if (binop_expr.binop == "<") {
-        lhs_val = generator->builder->CreateFCmpULT(lhs_val, rhs_val, "cmptmp");
-        value = generator->builder->CreateUIToFP(lhs_val,
+        lhs_val = context->builder.CreateFCmpULT(lhs_val, rhs_val, "cmptmp");
+        result = context->builder.CreateUIToFP(lhs_val,
                 llvm::Type::getDoubleTy(llvm::getGlobalContext()), "booltmp");
     } else {
         throw 0;
@@ -57,16 +123,11 @@ ValueGen::apply(const BinOpASTExpr &binop_expr)
 }
 
 void
-FunctionGen::apply(const ExternASTNode &double_expr)
+ValueGen::apply_to(const CallASTExpr &call_expr)
 {
-}
+    assert(result == nullptr);
 
-void
-ValueGen::apply(const CallASTExpr &call_expr)
-{
-    assert(value == nullptr);
-
-    llvm::Function* callee_func = generator->llvm_module->getFunction(call_expr.callee);
+    llvm::Function* callee_func = context->llvm_module->getFunction(call_expr.callee);
 
     if (!callee_func) throw -1;
 
@@ -74,12 +135,14 @@ ValueGen::apply(const CallASTExpr &call_expr)
 
     std::vector<llvm::Value*> arg_vals;
 
+    std::unique_ptr<ValueGen> vg;
     for (auto const &a : call_expr.args)
     {
-        ValueGen vg(generator);
-        a->inject(vg);
-        arg_vals.push_back(vg.get());
+        vg.reset(new ValueGen(context));
+        a->inject(*vg);
+        arg_vals.push_back(vg->extract());
     }
 
-    value = generator->builder->CreateCall(callee_func, arg_vals, "calltmp");
+    result = context->builder.CreateCall(callee_func, arg_vals, "calltmp");
 }
+
